5a9bceb7b53f9e92b112d0e261dfd6fbc7f6c545
Richard Mahoney <richard_mahoney@trimble.com>
2018-07-21 18:14:48 +0100

    General improvements

--- lunch.php
+++ lunch.php
@@ -2,12 +2,14 @@
 
 /**
  * Stress test (/bin/bash):
- * while :; do curl http://localhost:/lunch.php; done
+ * while :; do curl http://localhost/lunch.php; done
  * OR
  * while :; do php lunch.php; done
+ * OR to write to a file:
+ * while :; do php lunch.php >> output.txt; done
+ *
+ * However, it would be more efficient to run the code below in a loop rather than starting up the PHP binary every time.
  */
-
-top:
 
 /**
  * This function makes a simple random choice. At this time weightings are done by repetition. True weighting to come later.
@@ -22,18 +24,18 @@ function choose() {
                     'Itsu', 'Greggs', 'Greggs', 'Subway',
                     'Pret', 'McDonalds', 'Pret', 'Eat',
                     );
-    $randomDontBother = random_int(0,1); // Make 'Somewhere you don't like much' appear 50% less often... (disabled for now)
-    //$choice = random_int($randomDontBother, count($places) - 1);
+    //$randomFirstChoice = random_int(0,1); // Make $places[0] appear 50% less often... (disabled for now)
+    //$choice = random_int($randomFirstChoice, count($places) - 1);
     $choice = random_int(1, count($places) - 1);
     return $places[$choice];
 }
 
 $x = choose();
 
-if (strlen($x) == 0) {
-   goto top;
-}
-else {
+if (strlen($x) > 0) {
     echo $x . "\n";
     exit(0);
+}
+else {
+   exit(1);
 }
